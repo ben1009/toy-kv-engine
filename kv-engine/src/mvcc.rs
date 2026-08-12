0f1cc87535703cdea69796cae983d9901e1f69e8
ben1009 <liuhe1009@gmail.com>
2026-08-12 12:37:43 +0800

    Revert "perf: borrow point batch keys during mvcc staging"
    This reverts commit dfa419675885fbd47cae8c39a35a0b3e34e9ad17.

--- kv-engine/src/mvcc.rs
+++ kv-engine/src/mvcc.rs
@@ -330,7 +330,7 @@ impl LsmMvccInner {
     #[allow(clippy::type_complexity)]
     pub(crate) fn write_batch_wal_only(
         &self,
-        entries: &[(&[u8], bytes::Bytes, BatchEntryKind)],
+        entries: &[(bytes::Bytes, bytes::Bytes, BatchEntryKind)],
         memtable: &MemTable,
         shared_publish_bytes: bool,
     ) -> Result<(u64, DeferredBatchPublish, Option<u64>), anyhow::Error> {
